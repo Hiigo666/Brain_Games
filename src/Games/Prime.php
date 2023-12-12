6424423cafdccbc192890b21f859ac3fa61cf0ad
Evgenii.Goncharov <hichi411@gmail.com>
2023-12-12 20:17:01 +0600

    edit prime game

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -6,6 +6,9 @@ function prime()
 {
     $trueAnswer = "yes";
     $symbol = mt_rand(1, 20);
+    if ($symbol <= 1) {
+        $trueAnswer = "no";
+    }
     for ($i = 2; $i <= sqrt($symbol); $i++) {
         if ($symbol % $i == 0) {
             $trueAnswer = "no";
