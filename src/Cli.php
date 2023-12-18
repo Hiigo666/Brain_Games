7eeeb197fe9c57c6f90ef353d544ec95c3c52565
Evgenii.Goncharov <hichi411@gmail.com>
2023-12-18 21:20:53 +0600

    fix Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace Php\Project\Cli ;
+
+use function Cli\line;
+use function Cli\prompt;
+
+function greeting()
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt('May I have your name?', false, ' ');
+    line("Hello, %s!", $name);
+    return $name;
+}
