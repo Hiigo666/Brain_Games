280f041fef544d82e8042792064fffc09a9fa67d
Evgenii.Goncharov <hichi411@gmail.com>
2023-12-26 16:19:44 +0600

    edit Gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,20 +2,33 @@
 
 namespace Project\Games\Gcd;
 
+use function Project\Engine\examination;
+
+use const Project\Engine\COUNT;
+
+const DESCRIBE = "Find the greatest common divisor of given numbers.";
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 100;
+
 function gcd()
 {
-    $minNumber = 1;
-    $maxNumber = 100;
-    $number1 = mt_rand($minNumber, $maxNumber);
-    $number2 = mt_rand($minNumber, $maxNumber);
+    for ($i = 0; $i < COUNT; $i++) {
+    $number1 = mt_rand(MIN_NUMBER, MAX_NUMBER);
+    $number2 = mt_rand(MIN_NUMBER, MAX_NUMBER);
     $question = "{$number1} {$number2}";
     $maxNumber = max($number1, $number2);
     $nod = 1;
-    for ($i = 1; $i <= $maxNumber; $i++) {
-        if ($number1 % $i === 0 && $number2 % $i === 0) {
-            $nod = $i;
+    for ($j = 1; $j <= $maxNumber; $j++) {
+        if ($number1 % $j === 0 && $number2 % $j === 0) {
+            $nod = $j;
         }
     }
-    $return = [$question, $nod];
+    $return[] = [$question, $nod];
+}
     return $return;
+}
+
+function run()
+{
+    examination(gcd(), DESCRIBE);
 }
